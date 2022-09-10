5ec5fc7be548f9b98d3af7b867a0278af00cadc5
Luis <luisgallego778@gmail.com>
2022-09-10 22:11:22 +0200

    join data

--- test.py
+++ test.py
@@ -17,6 +17,9 @@ locations = list(dict.fromkeys(locations))
 
 base_url = r"https://power.larc.nasa.gov/api/temporal/hourly/point?parameters=WS50M&community=SB&longitude={longitude}&latitude={latitude}&start=20210602&end=20220529&format=json"
 
+wind_power_speed = pd.read_csv("wind_power_speed.csv",names=["date","power"],parse_dates=['date'])
+print(wind_power_speed)
+
 for latitude, longitude,name in locations[:60]:
         locations.remove((latitude, longitude,name))
         print(latitude, longitude,name)
@@ -25,11 +28,13 @@ for latitude, longitude,name in locations[:60]:
         data = {}
         response = requests.get(url=api_request_url, verify=True, timeout=30.00)
 
-        try:
-            content = json.loads(response.content.decode('utf-8'))['properties']['parameter']['WS50M']
-            attrs = []
-            vals = []
-            print(content)
-        except:
-            continue
+
+        content = json.loads(response.content.decode('utf-8'))['properties']['parameter']['WS50M']
+        df = pd.json_normalize(content).T
+        df = df.reset_index(level=0)
+        df.columns = ["date",name]
+        df["date"] = pd.to_datetime(df['date'],format="%Y%m%d%H")
+        print(df)
+        wind_power_speed = wind_power_speed.join(df.set_index('date'),on='date')
+        print(wind_power_speed.head(30))
         break
